2dcbd4f799c3b1ffac41c8251f2310e84014c69e
AbiUser <abiudayakumar11@gmail.com>
2023-10-06 12:52:54 +0100

    added entries route to app

--- server/app.js
+++ server/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const entryRouter = require('./routers/entries');
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 //middleware
-
+app.use("/entries", entryRouter)
 
 app.get('/', (req,res) => {
     res.send("This is Batman's diary, do NOT read or feel my wrath.")
